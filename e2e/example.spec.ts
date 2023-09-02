6bdc57d3de4872c8c2ca55825b77c5da256f7b25
Nick Rapattoni <rapattonimusic@me.com>
2023-09-02 07:11:20 -0700

    A test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Check the title of the website', async ({ page }) => {
+test('Hello I am a test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
