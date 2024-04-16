c73fdcf89f5dd451c5117373bbb67e189d19d3aa
Nick Rapattoni <rapattonimusic@me.com>
2024-04-16 12:42:03 -0700

    Update test01.spec.ts

--- e2e/test01.spec.ts
+++ e2e/test01.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
 
 test('This test will pass', async ({ page }) => {
-  console.log('Heleldfo swofffrfdflsfss!!!4')
+  console.log('Heleldfo swofffrftdflsfss!!!4')
 })
