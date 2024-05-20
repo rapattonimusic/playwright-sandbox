c3b8febd9b23708218de5d7e0e7bcbc77caebe9c
Nick Rapattoni <rapattonimusic@me.com>
2024-05-20 14:46:21 -0700

    Update test01.spec.ts

--- e2e/test01.spec.ts
+++ e2e/test01.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('This test will pass', async ({ page }) => {
-  console.log('Test be runninn!!')
+  console.log('Test be runnin!!')
   await page.goto('https://output.com/products/arcade');
   await page.waitForLoadState('domcontentloaded')
 })
