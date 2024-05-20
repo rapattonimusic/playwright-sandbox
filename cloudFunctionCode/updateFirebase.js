978b6bff7bd4c02f79a66ccc435b2a167c9ee76b
Nick Rapattoni <rapattonimusic@me.com>
2024-05-20 14:42:06 -0700

    Test

--- cloudFunctionCode/updateFirebase.js
+++ cloudFunctionCode/updateFirebase.js
@@ -9,7 +9,7 @@ admin.initializeApp({
 
 exports.updateFirebase = async (event, context) => {
   // Wait for 10 seconds
-  await new Promise(resolve => setTimeout(resolve, 10000))
+  await new Promise(resolve => setTimeout(resolve, 30000))
   const cbClient = new CloudBuildClient();
   const message = event.data
     ? JSON.parse(Buffer.from(event.data, 'base64').toString())
