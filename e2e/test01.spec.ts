15d43eb70028768d10187fc2e676ff80a6403005
Nick Rapattoni <rapattonimusic@me.com>
2024-05-20 13:46:05 -0700

    Test

--- e2e/test01.spec.ts
+++ e2e/test01.spec.ts
@@ -2,11 +2,6 @@ import { test, expect } from '@playwright/test'
 
 test('This test will pass', async ({ page }) => {
   console.log('Test be runnin!!')
-  await page.goto('https://output.com/');
   await page.goto('https://output.com/products/arcade');
   await page.waitForLoadState('domcontentloaded')
-  await page.getByRole('link', { name: 'Shop' }).click();
-  await page.locator('div').filter({ hasText: /^Learn$/ }).click();
-  await page.getByRole('link', { name: 'Company' }).click();
-  expect(2).toBe(3)
 })
