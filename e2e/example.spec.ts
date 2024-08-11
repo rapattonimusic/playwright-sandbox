7beb3395d132bd6af2690e72aed680c47aca21c4
Nick Rapattoni <rapattonimusic@me.com>
2024-08-11 06:47:08 -0700

    Testing it out

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('THIS SHOULD BE RUN 68', async ({ page }) => {
+test('This test will fail', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // await expect(page).toHaveTitle(/Playwright/);
+  // Fail the test
+  await expect(2).toBe(3);
 });
 
-test('Yes he did!', async ({ page }) => {
+test('This test will pass!', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -17,10 +19,3 @@ test('Yes he did!', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test.skip('OH HEEAAAAAOOOOOOOO YAAAA!', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-  await expect('1').toBe('2');
-});
