e27120edff0eac7a09a06d11d3ec14b33719bb39
Nick Rapattoni <rapattonimusic@me.com>
2025-01-04 07:46:00 -0800

    Update

--- e2e/test01.spec.ts
+++ e2e/test01.spec.ts
@@ -5,3 +5,8 @@ test('This test will pass', async ({ page }) => {
   await page.goto('https://output.com/products/arcade');
   await page.waitForLoadState('domcontentloaded')
 })
+
+test('This test will fail', async ({ page }) => {
+  console.log('Test be running1!!')
+  expect(1).toEqual(2)
+})
--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['line'],['html', { open: 'never', outputFolder: process.env.CI ? '/app/playwright-report' : './playwright-report' }]],
+  reporter: [
+    ['line'],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
